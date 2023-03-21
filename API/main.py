ab9f5b56a3e30b56fe8d1a90afd3b5311cf000b4
Everton Mutti Castro <84800212+EvertonMutti@users.noreply.github.com>
2023-03-21 09:37:46 -0300

    Update main.py

--- API/main.py
+++ API/main.py
@@ -4,7 +4,7 @@ Created on Sat Mar 18 19:26:24 2023
 
 @author: Everton
 """
-from Integracao.insert import create_product
+from routers.create_product import app
 
 if __name__ == "__main__":
-    uvicorn.run(create_product.app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
