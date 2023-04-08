a1b817526e3ae949205c286bbaa062e8411bd6fc
Everton Mutti <evertoncastro19@gmail.com>
2023-04-08 16:08:25 -0300

    testes unittarios

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -13,7 +13,7 @@ try:
         os.path.abspath(
             os.path.join(
                 os.path.dirname(__file__),
-                '../API/Integracao/insert'
+                '../API'
             )
         )
     )
@@ -22,26 +22,43 @@ except:
 
 import unittest
 from fastapi.testclient import TestClient
-from create_product import app
+from main import app
 
 client = TestClient(app)
 
 
-class TestProduct(unittest.TestCase):
+class TestAPI(unittest.TestCase):
+
     def test_create_product(self):
-        # Teste para criar um produto
-        product_data = {
-            "name": "Produto 1",
-            "description": "Descrição do Produto 1",
-            "code": "PRD-001",
+        response = client.post("/create/product/", json={
+            "name": "Product 1",
+            "description": "Product 1 description",
+            "code": "123456",
+            "empresa": "Company 1",
             "dimensions": "10x20x30",
             "value": 100.0
-        }
-        response = client.post("/create/product/", json=product_data)
+        })
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"message": "Ok"})
-    
-            
+
+    def test_select_product(self):
+        response = client.get("/select/products/1")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "id": 1,
+            "name": "Product 1",
+            "description": "Product 1 description",
+            "code": "123456",
+            "empresa": "Company 1",
+            "dimensions": "10x20x30",
+            "value": 100.0
+        })
+
+    def test_delete_product(self):
+        response = client.delete("/delete/products/1")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Product deleted"})
+
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main()
